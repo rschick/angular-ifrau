90ca8fbc0fddc12fb724fe6000422767dcc03e32
Russell Schick <Russell.Schick@d2l.com>
2015-07-21 14:03:15 -0400

    Initial version

---
+++ index.js
@@ -0,0 +1,63 @@
+angular.module('angular-ifrau', [
+])
+
+.provider('AppHost', function AppHostProvider() {
+
+	var conf = {};
+	var client;
+	var ifrau;
+	var connected = false;
+
+	function connect(_ifrau) {
+		ifrau = _ifrau;
+	}
+
+	this.connect = connect;
+
+	this.$get = function AppHostFactory($q) {
+
+		function AppHost() {
+
+			function connect() {
+
+				if (connected) {
+					return $q.when(conf);
+				}
+
+				var defer = $q.defer();
+				client = new ifrau.Client();
+				client.onEvent('cabinet.frapps.config', function(config) {
+					connected = true;
+					conf = _.assign(conf, config);
+					defer.resolve(conf);
+				});
+				client.connect();
+				return defer.promise;
+			}
+
+			function sendEvent(eventName, payload) {
+				client.sendEvent(eventName, payload);
+			}
+
+			function sendMessage(level, message) {
+				sendEvent('message', {
+					level: level,
+					message: message
+				});
+			}
+
+			function navigate(url) {
+				client.navigate(url);
+			}
+
+			this.sendEvent = sendEvent;
+			this.sendMessage = sendMessage;
+			this.conf = conf;
+			this.navigate = navigate;
+			this.connect = connect;
+		}
+
+		return new AppHost();
+	};
+});
+
